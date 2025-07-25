57fe08925049579b0bad56fb0793ea8a308c74b8
osiris-zyt <frankch2001@gmail.com>
2025-07-24 18:31:06 -0700

    minio operations and config

--- src/main/java/com/coursistant/lms/service/system/MinIOService.java
+++ src/main/java/com/coursistant/lms/service/system/MinIOService.java
@@ -1,47 +1,69 @@
 package com.coursistant.lms.service.system;
 
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.io.InputStream;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import io.minio.BucketExistsArgs;
+import io.minio.GetObjectArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
 import io.minio.UploadObjectArgs;
 import io.minio.errors.MinioException;
 
 public class MinIOService {
-    public void uploadFile() throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        try {
-      // Create a minioClient with the MinIO server playground, its access key and secret key.
-      MinioClient minioClient =
-          MinioClient.builder()
-              .endpoint("https://play.min.io")
-              .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-              .build();
 
-      // Make 'asiatrip' bucket if not exist.
-      boolean found =
-          minioClient.bucketExists(BucketExistsArgs.builder().bucket("asiatrip").build());
-      if (!found) {
-        // Make a new bucket called 'asiatrip'.
-        minioClient.makeBucket(MakeBucketArgs.builder().bucket("asiatrip").build());
-      } else {
-        System.out.println("Bucket 'asiatrip' already exists.");
-      }
 
-      minioClient.uploadObject(
-          UploadObjectArgs.builder()
-              .bucket("asiatrip")
-              .object("asiaphotos-2015.zip")
-              .filename("/home/user/Photos/asiaphotos.zip")
-              .build());
-      System.out.println(
-          "'/home/user/Photos/asiaphotos.zip' is successfully uploaded as "
-              + "object 'asiaphotos-2015.zip' to bucket 'asiatrip'.");
-    } catch (MinioException e) {
-      System.out.println("Error occurred: " + e);
-      System.out.println("HTTP trace: " + e.httpTrace());
+    private final MinioClient minioClient;
+
+    public MinIOService(MinioClient minioClient) {
+        this.minioClient = minioClient;
     }
+
+    // CREATE
+    public void uploadFile(String fileName, MultipartFile file, String bucket) throws Exception {
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+        }
+
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(fileName)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .build()
+        );
     }
+
+    // READ
+    public InputStream downloadFile(String fileName, String bucket) throws Exception {
+        return minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(fileName)
+                        .build()
+        );
+    }
+
+    // UPDATE
+    public void updateFile(String fileName, MultipartFile file, String bucket) throws Exception {
+        deleteFile(fileName, bucket); // optional
+        uploadFile(fileName, file, bucket);
+    }
+
+    // DELETE
+    public void deleteFile(String fileName, String bucket) throws Exception {
+        minioClient.removeObject(
+                RemoveObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(fileName)
+                        .build()
+        );
+    }
+
 }
