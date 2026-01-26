422c3d97787ff9c87ce143cfddbbe4b84d68b24a
Chshine1 <chshine@qq.com>
2026-01-27 00:27:06 +0800

    feat: Add a course controller with a course detail querying api

--- src/main/java/com/coursistant/lms/v2/controller/CourseController.java
+++ src/main/java/com/coursistant/lms/v2/controller/CourseController.java
@@ -1,8 +1,14 @@
 package com.coursistant.lms.v2.controller;
 
+import com.coursistant.lms.v2.common.ApiResponse;
+import com.coursistant.lms.v2.dto.CourseDetailDTO;
 import com.coursistant.lms.v2.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CourseController {
     private final CourseService courseService;
+
+    @GetMapping("/{courseId}/detail")
+    public ResponseEntity<ApiResponse<CourseDetailDTO>> getCourseDetail(@PathVariable Long courseId) {
+        // TODO: Structured logging (already annotated with Slf4j)
+        try {
+            CourseDetailDTO detail = courseService.getCourseDetail(courseId);
+            return ResponseEntity.ok(
+                    ApiResponse.success("Querying course detail success", detail)
+            );
+        } catch (RuntimeException e) {
+            // TODO: Domain exception encapsulation here
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ApiResponse.error(404, "Course doesn't exist"));
+        }
+    }
 }
