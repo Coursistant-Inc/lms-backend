0e6274301a38665761ee5478961e9bce5c3d2c65
Chshine1 <chshine@qq.com>
2026-01-27 16:36:27 +0800

    feat: Add create endpoints

--- src/main/java/com/coursistant/lms/v2/controller/CourseV2Controller.java
+++ src/main/java/com/coursistant/lms/v2/controller/CourseV2Controller.java
@@ -1,8 +1,7 @@
 package com.coursistant.lms.v2.controller;
 
 import com.coursistant.lms.v2.common.ApiResponse;
-import com.coursistant.lms.v2.dto.CourseDetailV2DTO;
-import com.coursistant.lms.v2.dto.UpdateCourseRequest;
+import com.coursistant.lms.v2.dto.*;
 import com.coursistant.lms.v2.service.CourseV2Service;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +18,9 @@ public class CourseV2Controller {
     private final CourseV2Service courseService;
 
     @GetMapping("/{courseId}/detail")
-    public ResponseEntity<ApiResponse<CourseDetailV2DTO>> getCourseDetail(@PathVariable Long courseId) {
+    public ResponseEntity<ApiResponse<CourseDetailV2DTO>> getCourseDetail(
+            @PathVariable Long courseId
+    ) {
         // TODO: Structured logging (already annotated with Slf4j)
         try {
             CourseDetailV2DTO detail = courseService.getCourseDetail(courseId);
@@ -33,10 +34,46 @@ public class CourseV2Controller {
         }
     }
 
+    @PostMapping("/new")
+    public ResponseEntity<ApiResponse<Long>> createCourse(
+            @RequestBody CreateCourseRequest request
+    ) {
+        // TODO: User id not implemented
+        var course = courseService.createCourse(1, request);
+        return ResponseEntity.ok(
+                ApiResponse.success("Creating course success", course.getId())
+        );
+    }
+
+    @PostMapping("/{courseId}/units/new")
+    public ResponseEntity<ApiResponse<Long>> createCourseUnit(
+            @PathVariable Long courseId,
+            @RequestBody CreateCourseUnitRequest request
+    ) {
+        var unit = courseService.createCourseUnit(courseId, request);
+        return ResponseEntity.ok(
+                ApiResponse.success("Creating course unit success", unit.getId())
+        );
+    }
+
+    @PostMapping("/{courseId}/units/{courseUnitId}/assignments/new")
+    public ResponseEntity<ApiResponse<Long>> createAssignment(
+            @PathVariable Long ignoredCourseId,
+            @PathVariable Long courseUnitId,
+            @RequestBody CreateAssignmentRequest request
+    ) {
+        // TODO: Also validate relation here
+        var assignment = courseService.createAssignment(courseUnitId, request);
+        return ResponseEntity.ok(
+                ApiResponse.success("Creating assignment success", assignment.getId())
+        );
+    }
+
     @PostMapping("/{courseId}/update")
     public ResponseEntity<ApiResponse<Long>> updateCourse(
             @PathVariable Long courseId,
-            @RequestBody @Valid UpdateCourseRequest request) {
+            @RequestBody @Valid UpdateCourseRequest request
+    ) {
         courseService.updateCourse(courseId, request);
         return ResponseEntity.ok(
                 ApiResponse.success("Updating course success", courseId)
