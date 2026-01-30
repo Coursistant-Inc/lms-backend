cacde22717ca0cab4b6e78b1a156e3754c0f51a2
Chshine1 <chshine@qq.com>
2026-01-30 20:20:13 +0800

    feat: Use lombok for Account class

--- src/main/java/com/coursistant/lms/entity/Account.java
+++ src/main/java/com/coursistant/lms/entity/Account.java
@@ -1,9 +1,10 @@
 package com.coursistant.lms.entity;
 
-/**
- * 角色用户父类
- * Base class for role-based users
- */
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
     private Integer id;
 
@@ -71,143 +72,4 @@ public class Account {
     transient private String rocketChatUserId;
 
     private String nwAccessToken;
-    public void setNwAccessToken(String nwAccessToken) {
-        this.nwAccessToken = nwAccessToken;
-    }
-
-    // getter 和 setter
-    public String getRocketChatToken() {
-        return rocketChatToken;
-    }
-
-    public void setRocketChatToken(String rocketChatToken) {
-        this.rocketChatToken = rocketChatToken;
-    }
-
-    public String getRocketChatUserId() {
-        return rocketChatUserId;
-    }
-
-    public void setRocketChatUserId(String rocketChatUserId) {
-        this.rocketChatUserId = rocketChatUserId;
-    }
-
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public String getInvitation() {
-        return invitation;
-    }
-
-    public void setInvitation(String invitation) {
-        this.invitation = invitation;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        if (email != null) {
-            this.email = email.trim().toLowerCase(); // 去除空格并转换为小写 / Trim spaces and convert to lowercase
-        }
-    }
-
-    public String getVerification() {
-        return verification;
-    }
-
-    public void setVerification(String verification) {
-        this.verification = verification;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-
-    
 }
